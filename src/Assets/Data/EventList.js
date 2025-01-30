6de9bd4ad306494d87a65d4a2af0ecd5ce243d34
Praneet Dixit <praneetdixit1234@gmail.com>
2025-01-30 18:23:01 +0530

    Add card layout

---
+++ src/Assets/Data/EventList.js
@@ -0,0 +1,98 @@
+import COS from "../Images/Career Orientation Session Photos/COS.jpg";
+import RGW from "../Images/Research Guidance Webinar Photos/RGW.jpg";
+import SA101 from "../Images/Study Abraod 101 Photos/SA101.jpg";
+
+const events = [
+    {
+      title: "Career Orientation Session",
+      description: "Student Welfare Group conducts the Career Orientation Session for 2nd and 3rd year students to resolve all their doubts regarding the different career opportunities based on different profiles like SDE, Data, Finance, Quant, Core, etc. and how to prepare to pursue them",
+      shortDescription: "Guidance on various career paths and how to prepare for them.",
+      image: COS
+    },
+    {
+      title: "Research Guidance Webinar",
+      description: "In this program we organize a panel discussion on the research domain as a career realm. It will disclose a roadmap to kickstart a journey in R&D as a career stream. Students who are oriented towards core field or they want to research upon some subjects which are in their department courses.",
+      shortDescription: "Insights and roadmap for a research-oriented career.",
+      image: RGW
+    },
+    {
+      title: "Study Abroad 101",
+      description: "An exclusive event where experts discuss the scope of international studies and provide relevant guidance for exams such as GMAT and GRE.",
+      shortDescription: "Guidance on studying abroad and exam preparation.",
+      image: SA101
+    },
+    {
+      title: "Off Campus Talks",
+      description: "To clarify all the doubts and provide a better picture of these off-campus opportunities, SWG conducts Off-Campus Talks with the panel being of students who have bagged good off-campus placements or internships.",
+      shortDescription: "Discussions on off-campus placements and internships.",
+      image: COS
+    },
+    {
+      title: "PDS Doubt Session",
+      description: "Student Welfare Group helps freshers in clearing all their doubts in the PDS course and help them out from the obstacles faced by them in learning programming through PDS doubt sessions. Seniors mentor them in these PDS sessions and tutorials will be provided to them to get even more clarity and practice.",
+      shortDescription: "Doubt clearing sessions for PDS with senior mentors.",
+      image: RGW
+    },
+    {
+      title: "Campus Tour",
+      description: "Campus ride is being organized to acquaint freshers about the important places of the campus that a student needs to know.",
+      shortDescription: "Guided tour to explore key campus locations.",
+      image: SA101
+    },
+    {
+      title: "SF Stall",
+      description: "A lively spot where folks can gather and enjoy quality time playing fantastic games, amidst all the pomp and flair of the Spring Fest!",
+      shortDescription: "A fun stall with games and activities at Spring Fest.",
+      image: COS
+    },
+    {
+      title: "ELAT",
+      description: "Student Welfare Group's ELAT initiative assesses your English proficiency, identifying improvement areas to help you achieve language mastery.",
+      shortDescription: "English proficiency assessment and improvement program.",
+      image: RGW
+    },
+    {
+      title: "Foresight",
+      description: "In this series we ask our seniors about their CDC placements and Internships and release it as blogs. In these blogs we cover about their experiences on their selection procedure, some insights into the work culture and some advice for all those who are appearing for CDC placement or Internships.",
+      shortDescription: "Blogs featuring experiences from CDC placements and internships.",
+      image: SA101
+    },
+    {
+      title: "Examania",
+      description: "In this program, we ask the students who scored a good CGPA and have aced their Academics in previous years. We ask them about their preparation strategies for exams. So that they can share their experience of exams in IIT Kharagpur with the freshers to perform well in their exams.",
+      shortDescription: "Exam preparation strategies from top students.",
+      image: COS
+    },
+    {
+      title: "DepC Talks",
+      description: "Student Welfare Group conducts its DepC talks to clear the doubts and queries related to the different departments, interdisciplinary courses and majorly about the popular Department Change procedure raised by the student community.",
+      shortDescription: "Guidance on department changes and interdisciplinary courses.",
+      image: RGW
+    },
+    {
+      title: "Academic Orientation Session",
+      description: "Student Welfare Group organizes a session to resolve all doubts about breadth, Minor, and Micro specialization and make freshers acquainted with all they need to know about Subject Registration procedure in subsequent years.",
+      shortDescription: "Overview of breadth, minors, and subject registration.",
+      image: SA101
+    },
+    {
+      title: "SWG App",
+      description: "One stop solution for all academic, non-academic and CDC doubts",
+      shortDescription: "All-in-one app for academic and career guidance.",
+      image: COS
+    },
+    {
+      title: "Student Mentorship Program",
+      description: "Under this program, every 1st-year student pairs with a 3rd or 4th-year mentor in their department for campus and academic guidance.",
+      shortDescription: "Mentorship program pairing juniors with senior mentors.",
+      image: RGW
+    },
+    {
+      title: "UPSC Talks",
+      description: "Simplifying the toughest exam with insights from past aspirants.",
+      shortDescription: "Guidance and insights from UPSC aspirants.",
+      image: SA101
+    },
+  ];
+  
+  export default events;
--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './Events.css'
+import events from '../../Assets/Data/EventList'
 
 export default function Events() {
   return (
-    <div id='eventsWrapper'>Events</div>
+    <>
+    <div id='eventsWrapper'>
+      <h2>Events</h2>
+      <div id='events'>
+        {events.map((event, index) => (
+          <div className='event' key={index}>
+            <img src={event.image} alt={event.title} />
+            <h3>{event.title}</h3>
+            <p>{event.shortDescription}</p>
+            </div>
+        ))}
+        </div>
+    </div>
+    </>
   )
 }
