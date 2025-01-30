b15f54156bd8f72d73f517ceb0a7deb9d2e51186
Praneet Dixit <praneetdixit1234@gmail.com>
2025-01-30 19:50:47 +0530

    Edit card layout

--- src/Assets/Data/EventList.js
+++ src/Assets/Data/EventList.js
@@ -1,6 +1,17 @@
 import COS from "../Images/Career Orientation Session Photos/COS.jpg";
 import RGW from "../Images/Research Guidance Webinar Photos/RGW.jpg";
 import SA101 from "../Images/Study Abraod 101 Photos/SA101.jpg";
+import OffCampus from "../Images/Off Campus Talks Photos/OffCampus.jpg";
+import PDS from "../Images/PDS Doubt Session Photos/PDS.jpg";
+import CampusTour from "../Images/Campus Tour Photos/CampusTour.png";
+import SF from "../Images/SF Stall Photos/SF.jpg";
+import ELAT from "../Images/ELAT Photos/ELAT.png";
+import Foresight from "../Images/Foresight Photos/Foresight.png";
+import Examania from "../Images/Examania Photos/Examania.jpg";
+import AOW from "../Images/AOW/AOW.png";
+import SWGApp from "../Images/SWG App/SWGApp.png";
+import SMP from "../Images/SMP/SMP.jpeg";
+import UPSC from "../Images/UPSC/UPSC.png";
 
 const events = [
     {
@@ -25,73 +36,67 @@ const events = [
       title: "Off Campus Talks",
       description: "To clarify all the doubts and provide a better picture of these off-campus opportunities, SWG conducts Off-Campus Talks with the panel being of students who have bagged good off-campus placements or internships.",
       shortDescription: "Discussions on off-campus placements and internships.",
-      image: COS
+      image: OffCampus
     },
     {
       title: "PDS Doubt Session",
       description: "Student Welfare Group helps freshers in clearing all their doubts in the PDS course and help them out from the obstacles faced by them in learning programming through PDS doubt sessions. Seniors mentor them in these PDS sessions and tutorials will be provided to them to get even more clarity and practice.",
       shortDescription: "Doubt clearing sessions for PDS with senior mentors.",
-      image: RGW
+      image: PDS
     },
     {
       title: "Campus Tour",
       description: "Campus ride is being organized to acquaint freshers about the important places of the campus that a student needs to know.",
       shortDescription: "Guided tour to explore key campus locations.",
-      image: SA101
+      image: CampusTour
     },
     {
       title: "SF Stall",
       description: "A lively spot where folks can gather and enjoy quality time playing fantastic games, amidst all the pomp and flair of the Spring Fest!",
       shortDescription: "A fun stall with games and activities at Spring Fest.",
-      image: COS
+      image: SF
     },
     {
       title: "ELAT",
       description: "Student Welfare Group's ELAT initiative assesses your English proficiency, identifying improvement areas to help you achieve language mastery.",
       shortDescription: "English proficiency assessment and improvement program.",
-      image: RGW
+      image: ELAT
     },
     {
       title: "Foresight",
       description: "In this series we ask our seniors about their CDC placements and Internships and release it as blogs. In these blogs we cover about their experiences on their selection procedure, some insights into the work culture and some advice for all those who are appearing for CDC placement or Internships.",
       shortDescription: "Blogs featuring experiences from CDC placements and internships.",
-      image: SA101
+      image: Foresight
     },
     {
       title: "Examania",
       description: "In this program, we ask the students who scored a good CGPA and have aced their Academics in previous years. We ask them about their preparation strategies for exams. So that they can share their experience of exams in IIT Kharagpur with the freshers to perform well in their exams.",
       shortDescription: "Exam preparation strategies from top students.",
-      image: COS
-    },
-    {
-      title: "DepC Talks",
-      description: "Student Welfare Group conducts its DepC talks to clear the doubts and queries related to the different departments, interdisciplinary courses and majorly about the popular Department Change procedure raised by the student community.",
-      shortDescription: "Guidance on department changes and interdisciplinary courses.",
-      image: RGW
+      image: Examania
     },
     {
       title: "Academic Orientation Session",
       description: "Student Welfare Group organizes a session to resolve all doubts about breadth, Minor, and Micro specialization and make freshers acquainted with all they need to know about Subject Registration procedure in subsequent years.",
       shortDescription: "Overview of breadth, minors, and subject registration.",
-      image: SA101
+      image: AOW
     },
     {
       title: "SWG App",
       description: "One stop solution for all academic, non-academic and CDC doubts",
       shortDescription: "All-in-one app for academic and career guidance.",
-      image: COS
+      image: SWGApp
     },
     {
       title: "Student Mentorship Program",
       description: "Under this program, every 1st-year student pairs with a 3rd or 4th-year mentor in their department for campus and academic guidance.",
       shortDescription: "Mentorship program pairing juniors with senior mentors.",
-      image: RGW
+      image: SMP
     },
     {
       title: "UPSC Talks",
       description: "Simplifying the toughest exam with insights from past aspirants.",
       shortDescription: "Guidance and insights from UPSC aspirants.",
-      image: SA101
+      image: UPSC
     },
   ];
   
--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
-import './Events.css'
-import events from '../../Assets/Data/EventList'
+import React from "react";
+import "./Events.css";
+import events from "../../Assets/Data/EventList";
+import { motion } from "framer-motion";
 
 export default function Events() {
-  return (
-    <>
-    <div id='eventsWrapper'>
-      <h2>Events</h2>
-      <div id='events'>
-        {events.map((event, index) => (
-          <div className='event' key={index}>
-            <img src={event.image} alt={event.title} />
-            <h3>{event.title}</h3>
-            <p>{event.shortDescription}</p>
+    const hoverVariants = {
+        hover: {
+            scale: 1.02,
+            y: -5,
+            boxShadow: "0 2px 5px 0 #028d93",
+        },
+    };
+
+    const imgHoverVariants = {
+        hover: {
+            padding: "15px",
+            borderRadius: "20px",
+        },
+    };
+    return (
+        <>
+            <div id="eventsWrapper">
+                <h2>Events</h2>
+                <div id="events">
+                    {events.map((event, index) => (
+                        <motion.div
+                            className="event"
+                            key={index}
+                            variants={hoverVariants}
+                            whileHover="hover"
+                            transition={{ duration: 0.2 }}
+                        >
+                            <motion.img
+                                src={event.image}
+                                alt={event.title}
+                                variants={imgHoverVariants}
+                            />
+                            <div className="eventDetail">
+                                <h3>{event.title}</h3>
+                                <p>{event.shortDescription}</p>
+                            </div>
+                        </motion.div>
+                    ))}
+                </div>
             </div>
-        ))}
-        </div>
-    </div>
-    </>
-  )
+        </>
+    );
 }
