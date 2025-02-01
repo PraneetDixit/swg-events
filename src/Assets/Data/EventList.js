87b6fe4ce6b357dc561317d049b6c010e713ae2e
Harsh Raj Dubey <90445942+harshrajdubey@users.noreply.github.com>
2025-02-01 23:18:07 +0530

    ui improvements and event json changed

--- src/Assets/Data/EventList.js
+++ src/Assets/Data/EventList.js
@@ -1,103 +1,202 @@
 import COS from "../Images/Career Orientation Session Photos/COS.jpg";
+import cos1 from "../Images/Career Orientation Session Photos/cos1 (1).jpg";
+import cos2 from "../Images/Career Orientation Session Photos/cos1 (2).jpg";
+import cos3 from "../Images/Career Orientation Session Photos/cos1 (3).jpg";
+import cos4 from "../Images/Career Orientation Session Photos/cos1 (4).jpg";
+import cos5 from "../Images/Career Orientation Session Photos/cos1 (5).jpg";
+import cos6 from "../Images/Career Orientation Session Photos/cos1 (6).jpg";
+import cos7 from "../Images/Career Orientation Session Photos/cos1 (7).jpg";
+import cos8 from "../Images/Career Orientation Session Photos/cos1 (8).jpg";
+
 import RGW from "../Images/Research Guidance Webinar Photos/RGW.jpg";
-import SA101 from "../Images/Study Abraod 101 Photos/SA101.jpg";
+import rgw1 from "../Images/Research Guidance Webinar Photos/rgw1 (1).png";
+import rgw2 from "../Images/Research Guidance Webinar Photos/rgw1 (2).png";
+import rgw3 from "../Images/Research Guidance Webinar Photos/rgw1 (3).png";
+
+import SA101 from "../Images/Study Abroad 101 Photos/SA101.jpg";
+
 import OffCampus from "../Images/Off Campus Talks Photos/OffCampus.jpg";
+import oc1 from "../Images/Off Campus Talks Photos/oc1 (1).jpg";
+import oc2 from "../Images/Off Campus Talks Photos/oc1 (2).jpg";
+import oc3 from "../Images/Off Campus Talks Photos/oc1 (3).jpg";
+
 import PDS from "../Images/PDS Doubt Session Photos/PDS.jpg";
+import pds1 from "../Images/PDS Doubt Session Photos/pds1 (1).jpg";
+import pds2 from "../Images/PDS Doubt Session Photos/pds1 (2).jpg";
+import pds3 from "../Images/PDS Doubt Session Photos/pds1 (3).jpg";
+import pds4 from "../Images/PDS Doubt Session Photos/pds1 (4).jpg";
+import pds5 from "../Images/PDS Doubt Session Photos/pds1 (5).jpg";
+import pds6 from "../Images/PDS Doubt Session Photos/pds1 (6).jpg";
+import pds7 from "../Images/PDS Doubt Session Photos/pds1 (7).jpg";
+import pds8 from "../Images/PDS Doubt Session Photos/pds1 (8).jpg";
+import pds9 from "../Images/PDS Doubt Session Photos/pds1 (9).jpg";
+
 import CampusTour from "../Images/Campus Tour Photos/CampusTour.png";
+import ct1 from "../Images/Campus Tour Photos/ct1 (1).jpg";
+import ct2 from "../Images/Campus Tour Photos/ct1 (2).jpg";
+import ct3 from "../Images/Campus Tour Photos/ct1 (3).jpg";
+import ct4 from "../Images/Campus Tour Photos/ct1 (4).jpg";
+import ct5 from "../Images/Campus Tour Photos/ct1 (5).jpg";
+import ct6 from "../Images/Campus Tour Photos/ct1 (6).jpg";
+import ct7 from "../Images/Campus Tour Photos/ct1 (7).jpg";
+import ct8 from "../Images/Campus Tour Photos/ct1 (8).jpg";
+import ct9 from "../Images/Campus Tour Photos/ct1 (9).jpg";
+
 import SF from "../Images/SF Stall photos/SF.jpg";
+import sf1 from "../Images/SF Stall photos/sf1 (1).jpg";
+import sf2 from "../Images/SF Stall photos/sf1 (2).jpg";
+import sf3 from "../Images/SF Stall photos/sf1 (3).jpg";
+import sf4 from "../Images/SF Stall photos/sf1 (4).jpg";
+import sf5 from "../Images/SF Stall photos/sf1 (5).jpg";
+import sf6 from "../Images/SF Stall photos/sf1 (6).jpg";
+import sf7 from "../Images/SF Stall photos/sf1 (7).jpg";
+import sf8 from "../Images/SF Stall photos/sf1 (8).jpg";
+import sf9 from "../Images/SF Stall photos/sf1 (9).jpg";
+
 import ELAT from "../Images/ELAT Photos/ELAT.png";
+import elat1 from "../Images/ELAT photos/elat1 (1).jpg";
+import elat2 from "../Images/ELAT photos/elat1 (2).jpg";
+
 import Foresight from "../Images/Foresight Photos/Foresight.png";
+import fore1 from "../Images/Foresight Photos/fore1 (1).png";
+import fore2 from "../Images/Foresight Photos/fore1 (2).png";
+import fore3 from "../Images/Foresight Photos/fore1 (3).png";
+import fore4 from "../Images/Foresight Photos/fore1 (4).png";
+
 import Examania from "../Images/Examania Photos/Examania.jpg";
+import exam1 from "../Images/Examania Photos/exam1 (1).jpg";
+import exam2 from "../Images/Examania Photos/exam1 (2).jpg";
+import exam3 from "../Images/Examania Photos/exam1 (3).jpg";
+
 import AOW from "../Images/AOW/AOW.png";
+import aow1 from "../Images/AOW/aow1 (1).png";
+import aow2 from "../Images/AOW/aow1 (2).png";
+
 import SWGApp from "../Images/SWG app/SWGApp.png";
+import swg1 from "../Images/SWG app/swg1 (1).png";
+import swg2 from "../Images/SWG app/swg1 (2).png";
+import swg3 from "../Images/SWG app/swg1 (3).png";
+
 import SMP from "../Images/SMP/SMP.jpeg";
+import smp1 from "../Images/SMP/smp1 (1).jpeg";
+import smp2 from "../Images/SMP/smp1 (2).jpeg";
+import smp3 from "../Images/SMP/smp1 (3).jpeg";
+
 import UPSC from "../Images/UPSC/UPSC.png";
+import upsc1 from "../Images/UPSC/upsc1 (1).png";
+import upsc2 from "../Images/UPSC/upsc1 (2).png";
 
 const events = [
-    {
-      title: "Career Orientation Session",
-      description: "Student Welfare Group conducts the Career Orientation Session for 2nd and 3rd year students to resolve all their doubts regarding the different career opportunities based on different profiles like SDE, Data, Finance, Quant, Core, etc. and how to prepare to pursue them",
-      shortDescription: "Guidance on various career paths and how to prepare for them.",
-      image: COS
-    },
-    {
-      title: "Research Guidance Webinar",
-      description: "In this program we organize a panel discussion on the research domain as a career realm. It will disclose a roadmap to kickstart a journey in R&D as a career stream. Students who are oriented towards core field or they want to research upon some subjects which are in their department courses.",
-      shortDescription: "Insights and roadmap for a research-oriented career.",
-      image: RGW
-    },
-    {
-      title: "Study Abroad 101",
-      description: "An exclusive event where experts discuss the scope of international studies and provide relevant guidance for exams such as GMAT and GRE.",
-      shortDescription: "Guidance on studying abroad and exam preparation.",
-      image: SA101
-    },
-    {
-      title: "Off Campus Talks",
-      description: "To clarify all the doubts and provide a better picture of these off-campus opportunities, SWG conducts Off-Campus Talks with the panel being of students who have bagged good off-campus placements or internships.",
-      shortDescription: "Discussions on off-campus placements and internships.",
-      image: OffCampus
-    },
-    {
-      title: "PDS Doubt Session",
-      description: "Student Welfare Group helps freshers in clearing all their doubts in the PDS course and help them out from the obstacles faced by them in learning programming through PDS doubt sessions. Seniors mentor them in these PDS sessions and tutorials will be provided to them to get even more clarity and practice.",
-      shortDescription: "Doubt clearing sessions for PDS with senior mentors.",
-      image: PDS
-    },
-    {
-      title: "Campus Tour",
-      description: "Campus ride is being organized to acquaint freshers about the important places of the campus that a student needs to know.",
-      shortDescription: "Guided tour to explore key campus locations.",
-      image: CampusTour
-    },
-    {
-      title: "SF Stall",
-      description: "A lively spot where folks can gather and enjoy quality time playing fantastic games, amidst all the pomp and flair of the Spring Fest!",
-      shortDescription: "A fun stall with games and activities at Spring Fest.",
-      image: SF
-    },
-    {
-      title: "ELAT",
-      description: "Student Welfare Group's ELAT initiative assesses your English proficiency, identifying improvement areas to help you achieve language mastery.",
-      shortDescription: "English proficiency assessment and improvement program.",
-      image: ELAT
-    },
-    {
-      title: "Foresight",
-      description: "In this series we ask our seniors about their CDC placements and Internships and release it as blogs. In these blogs we cover about their experiences on their selection procedure, some insights into the work culture and some advice for all those who are appearing for CDC placement or Internships.",
-      shortDescription: "Blogs featuring experiences from CDC placements and internships.",
-      image: Foresight
-    },
-    {
-      title: "Examania",
-      description: "In this program, we ask the students who scored a good CGPA and have aced their Academics in previous years. We ask them about their preparation strategies for exams. So that they can share their experience of exams in IIT Kharagpur with the freshers to perform well in their exams.",
-      shortDescription: "Exam preparation strategies from top students.",
-      image: Examania
-    },
-    {
-      title: "Academic Orientation Session",
-      description: "Student Welfare Group organizes a session to resolve all doubts about breadth, Minor, and Micro specialization and make freshers acquainted with all they need to know about Subject Registration procedure in subsequent years.",
-      shortDescription: "Overview of breadth, minors, and subject registration.",
-      image: AOW
-    },
-    {
-      title: "SWG App",
-      description: "One stop solution for all academic, non-academic and CDC doubts",
-      shortDescription: "All-in-one app for academic and career guidance.",
-      image: SWGApp
-    },
-    {
-      title: "Student Mentorship Program",
-      description: "Under this program, every 1st-year student pairs with a 3rd or 4th-year mentor in their department for campus and academic guidance.",
-      shortDescription: "Mentorship program pairing juniors with senior mentors.",
-      image: SMP
-    },
-    {
-      title: "UPSC Talks",
-      description: "Simplifying the toughest exam with insights from past aspirants.",
-      shortDescription: "Guidance and insights from UPSC aspirants.",
-      image: UPSC
-    },
-  ];
-  
-  export default events;
+  {
+    title: "Career Orientation Session",
+    description:
+      "Student Welfare Group conducts the Career Orientation Session for 2nd and 3rd year students to resolve all their doubts regarding the different career opportunities based on different profiles like SDE, Data, Finance, Quant, Core, etc. and how to prepare to pursue them",
+    shortDescription: "Guidance on various career paths and how to prepare for them.",
+    image: COS,
+    additionalimage: [COS, cos1, cos2, cos3, cos4, cos5, cos6, cos7, cos8],
+  },
+  {
+    title: "Research Guidance Webinar",
+    description:
+      "In this program we organize a panel discussion on the research domain as a career realm. It will disclose a roadmap to kickstart a journey in R&D as a career stream. Students who are oriented towards core field or they want to research upon some subjects which are in their department courses.",
+    shortDescription: "Insights and roadmap for a research-oriented career.",
+    image: RGW,
+    additionalimage: [RGW, rgw1, rgw2, rgw3],
+  },
+  {
+    title: "Study Abroad 101",
+    description:
+      "An exclusive event where experts discuss the scope of international studies and provide relevant guidance for exams such as GMAT and GRE.",
+    shortDescription: "Guidance on studying abroad and exam preparation.",
+    image: SA101,
+    additionalimage: [SA101],
+  },
+  {
+    title: "Off Campus Talks",
+    description:
+      "To clarify all the doubts and provide a better picture of these off-campus opportunities, SWG conducts Off-Campus Talks with the panel being of students who have bagged good off-campus placements or internships.",
+    shortDescription: "Discussions on off-campus placements and internships.",
+    image: OffCampus,
+    additionalimage: [OffCampus, oc1, oc2, oc3],
+  },
+  {
+    title: "PDS Doubt Session",
+    description:
+      "Student Welfare Group helps freshers in clearing all their doubts in the PDS course and help them out from the obstacles faced by them in learning programming through PDS doubt sessions. Seniors mentor them in these PDS sessions and tutorials will be provided to them to get even more clarity and practice.",
+    shortDescription: "Doubt clearing sessions for PDS with senior mentors.",
+    image: PDS,
+    additionalimage: [PDS, pds1, pds2, pds3, pds4, pds5, pds6, pds7, pds8, pds9],
+  },
+  {
+    title: "Campus Tour",
+    description:
+      "Campus ride is being organized to acquaint freshers about the important places of the campus that a student needs to know.",
+    shortDescription: "Guided tour to explore key campus locations.",
+    image: CampusTour,
+    additionalimage: [CampusTour, ct1, ct2, ct3, ct4, ct5, ct6, ct7, ct8, ct9],
+  },
+  {
+    title: "SF Stall",
+    description:
+      "A lively spot where folks can gather and enjoy quality time playing fantastic games, amidst all the pomp and flair of the Spring Fest!",
+    shortDescription: "A fun stall with games and activities at Spring Fest.",
+    image: SF,
+    additionalimage: [SF, sf1, sf2, sf3, sf4, sf5, sf6, sf7, sf8, sf9],
+  },
+  {
+    title: "ELAT",
+    description:
+      "Student Welfare Group's ELAT initiative assesses your English proficiency, identifying improvement areas to help you achieve language mastery.",
+    shortDescription: "English proficiency assessment and improvement program.",
+    image: ELAT,
+    additionalimage: [ELAT, elat1, elat2],
+  },
+  {
+    title: "Foresight",
+    description:
+      "In this series we ask our seniors about their CDC placements and Internships and release it as blogs. In these blogs we cover about their experiences on their selection procedure, some insights into the work culture and some advice for all those who are appearing for CDC placement or Internships.",
+    shortDescription: "Blogs featuring experiences from CDC placements and internships.",
+    image: Foresight,
+    additionalimage: [Foresight, fore1, fore2, fore3, fore4],
+  },
+  {
+    title: "Examania",
+    description:
+      "In this program, we ask the students who scored a good CGPA and have aced their Academics in previous years. We ask them about their preparation strategies for exams. So that they can share their experience of exams in IIT Kharagpur with the freshers to perform well in their exams.",
+    shortDescription: "Exam preparation strategies from top students.",
+    image: Examania,
+    additionalimage: [Examania, exam1, exam2, exam3],
+  },
+  {
+    title: "Academic Orientation Session",
+    description:
+      "Student Welfare Group organizes a session to resolve all doubts about breadth, Minor, and Micro specialization and make freshers acquainted with all they need to know about Subject Registration procedure in subsequent years.",
+    shortDescription: "Overview of breadth, minors, and subject registration.",
+    image: AOW,
+    additionalimage: [AOW, aow1, aow2],
+  },
+  {
+    title: "SWG App",
+    description: "One stop solution for all academic, non-academic and CDC doubts",
+    shortDescription: "All-in-one app for academic and career guidance.",
+    image: SWGApp,
+    additionalimage: [SWGApp, swg1, swg2, swg3],
+  },
+  {
+    title: "Student Mentorship Program",
+    description:
+      "Under this program, every 1st-year student pairs with a 3rd or 4th-year mentor in their department for campus and academic guidance.",
+    shortDescription: "Mentorship program pairing juniors with senior mentors.",
+    image: SMP,
+    additionalimage: [SMP, smp1, smp2, smp3],
+  },
+  {
+    title: "UPSC Talks",
+    description: "Simplifying the toughest exam with insights from past aspirants.",
+    shortDescription: "Guidance and insights from UPSC aspirants.",
+    image: UPSC,
+    additionalimage: [UPSC, upsc1, upsc2],
+  },
+];
+
+export default events;
